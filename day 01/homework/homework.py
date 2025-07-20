2c37993851c317e21bf612f9454cccea4707ccd8
levani936 <le.bedoshvili@yahoo.com>
2025-07-20 07:55:53 -0700

    commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,16 @@
+car = "Mercedes"
+Country = "Canada"
+State = "Texas"
+Planet = "Saturn"
+
+
+Mother = "Nana"
+Father = "David"
+Grandmother = "Qetevan"
+Grandfather = "Bidzina"
+
+# print(Mother)
+# print(Father)
+# print(Grandmother)
+# print(Grandfather)
+print(Mother , Father , Grandmother , Grandfather)
