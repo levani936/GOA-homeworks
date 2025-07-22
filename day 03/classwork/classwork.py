4c09b767cc84732b57e0e876146f24dd88a1003d
levani936 <le.bedoshvili@yahoo.com>
2025-07-22 07:42:37 -0700

    commit

---
+++ day 03/classwork/classwork.py
@@ -0,0 +1,22 @@
+number = 14
+number2 = 10.9
+print(number + number2)
+
+string = "hello"
+string2 = "world"
+print(string + string2)
+
+print(5 + 5)
+print(2 + 8)
+
+print(9 * 9)
+print(8 * 6)
+
+print(7 - 6)
+print(68 - 42)
+
+print(80 / 4)
+print(60 / 3)
+
+print(90 // 30)
+print(999 // 3)
