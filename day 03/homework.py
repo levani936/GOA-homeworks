b7acb8f03abe343fc497a8a19d9a7b85bd25f497
levani936 <le.bedoshvili@yahoo.com>
2025-07-22 07:04:02 -0700

    commit

---
+++ day 03/homework.py
@@ -0,0 +1,36 @@
+num1 = 33
+num2 = 77
+num3 = 50
+print(num1 + num2 + num3)
+
+x = 10
+y = 90
+print(x + y)
+
+lenght = 20
+width = 10
+print(lenght * 2 + width * 2)
+
+print(2 + 8)
+print(77 + 23)
+print(50 + 19)
+print(99 + 11)
+print(42 + 77)
+
+print(100 - 89)
+print(500 - 250)
+print(777 - 555)
+print(70 - 35)
+print(1000 - 1)
+
+print(8 * 8)
+print(44 * 2)
+print(7 * 9)
+print(50 * 4)
+print(750 * 3)
+
+print(10 / 2)
+print(100 / 50)
+print(200 / 50)
+print(1000 / 2)
+print(2050 / 2)
