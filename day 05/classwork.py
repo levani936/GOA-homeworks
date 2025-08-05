3ba8d40c87ff86d62f64c50439ec5a0749b52772
levani936 <le.bedoshvili@yahoo.com>
2025-08-05 03:29:04 -0700

    commit

---
+++ day 05/classwork.py
@@ -0,0 +1,49 @@
+
+# debugging არის კოდირების დროს დაშვებული შეცდომების შენიშვნა/გამოსწორება პროგრამს მიერ.
+
+#python არის case sensitive ენა, რაც ნიშნავს რომ მაგალითად home და Home არ არის ერთი და იგივე, ანუ ორივე განსხვავებულია.
+#python case sensitive ენაა, რადგან ცვლადების წერის დროს Error არ გამოიტანოს.
+
+number_one = 90
+print(number_one)
+
+food_one = "shawarma"
+print(food_one)
+
+favourite_car = "buggati"
+print(favourite_car)
+
+our_country = "georgia"
+print(our_country)
+
+planet_one = "jupiter"
+print(planet_one)
+
+# 2people = singing
+#ცვლადი არ შეიძლება იწყებოდეს რიცხვით
+# word - hello
+#ცვლადი უნდა შევქმნათ ტოლობის გამოყენებით
+# something =
+# ცვლადი არ არის მითითებული
+# = car
+# ცვლადის "ყუთი" არ არის შექმნილი
+# number = "9"
+# 9 ინტეგერია და ბჭყალები არ სჭირდება
+
+num1 = 10
+num2 = 90
+num3 = num1 + num2
+print(num3)
+num1 = 77
+num2 = 66
+num3 = num1 - num2
+print(num3)
+num1 = 7
+num2 = 9
+num3 = num1 * num2
+print(num3)
+num1 = 60
+num2 = 3
+num3 = num1 / num2
+print(num3)
+
---
+++ day 05/homework.py
@@ -0,0 +1,55 @@
+#კომენტარი არის კარგი პრაქტიკა ადამიანებისთვის, რადგან ის მხოლოდ ადამიანებისთვისაა და შეგვიძლია მივხვდეთ, თუ რა წერია კოდში.პროგრამა კი მას აიგნორებს.
+
+my_name = "Levani"
+print(my_name)
+my_surname = "Bedoshvili"
+print(my_surname)
+my_hometown = "kaspi"
+print(my_hometown)
+my_hobby = "swimming"
+print(my_hobby)
+my_grade = "6th"
+print(my_grade)
+my_favsubject = "geography"
+print(my_favsubject)
+country_ilivein = "Georgia"
+print(country_ilivein)
+my_favfood = "shawarma"
+print(my_favfood)
+my_favdrink = "cola"
+print(my_favdrink)
+my_favweather = "cloudy"
+print(my_favweather)
+
+name = "Luka"
+age_num = " 56"
+my_favmov = " blood_and_bone"
+my_friend_name = " Nika"
+
+print ("my name is" + name)
+print("my age is" + age_num)
+print("my favourite movie is" + my_favmov)
+print("my friend's name is" + my_friend_name)
+
+budget1 = 550
+budget2 =  1250
+new_budget = budget1 + budget2
+print(new_budget)
+
+name = "Levani"
+surname = " Bedoshvili"
+person = name + surname
+print(person)
+
+word = "Hello"
+word2 = " World"
+final_word = word + word2
+print(final_word)
+
+Name = "Kratos"
+status = " the god of war"
+title = Name + status
+print(title)
+
+# snake_case არის ცვლადი, რომელსაც გამოტოვების ნაცვლად ქვედა ტირეს იყენებს.
+# გამოიყენება რამდენიმე სიტყვიანი ცვლადების შექმნისთვის
