c8e112b9e127f4a23be6b3f1a33fcb08ab97ea14
levani936 <le.bedoshvili@yahoo.com>
2025-08-23 00:48:46 -0700

    commit

---
+++ day 07/classwork.py
@@ -0,0 +1,33 @@
+#Input არის რაც ხდება პროგრამის შიგნით.
+#Output არის რაც ხდება პროგრამის გარეთ input-ის მეშვეობით.
+# input.
+print("hello") 
+# Output- ჩნდება Terminalში.
+
+# კონკატინაცია ხდება როცა ორ stringს ვაერთებთ +ის გამოყენებით.
+name = input("Levani")
+print(name)
+
+name1 = 'Levani'
+surname = " Bedoshvili"
+age = " 12"
+ilivein = " Kaspi"
+favcolour = " green"
+favcar = " Buggati"
+print( name1 + surname + age + ilivein + favcolour + favcar)
+
+print(str(5))
+print(str(2382))
+print(str(69))
+print(str(420))
+print(str(9292929))
+
+print(int("a"))
+print(int("Hello"))
+print(int("house"))
+print(int("Town"))
+print(int("Food"))
+
+num = input(int("70"))
+print(num * 15)
+
---
+++ day 07/homework.py
@@ -0,0 +1,29 @@
+# Input არის ის,რასაც ჩვენ ვწერთ პროგრამაში რათა პროგრამამ იგი შეასრულოს.
+# input ს რომ ვაკეთებთ დანარჩენი უნდა ჩავწეროთ ტერმინალში ან ჩავწეროთ კოდის წერის დროს.
+# Town = input("kaspi")
+
+# output არის ის რაც პროგრამას გამოაქვს Input-ში ჩაწერილის მიხედვით.Output გამოდის ტერმინალში.
+
+# კონკატინაცია ხდება მაშინ როცა ორ string-ს ვაერთებთ ერთად + ის გამოყენებით.
+
+integer = 7
+string = "7"
+print(integer + int(string))
+print(integer * int(string))
+print(integer / int(string))
+print(integer // int(string))
+
+print(float(1.72))
+
+# Data convertion არის სიტყვის/ციფრის სხვა მონაცემთა ტიპში გადაყვანა.
+# print(str(5))
+
+# Implicitly არის როცა ჩვენვე მივუთითებთ პროგრამას თუ რა მონაცემთა ტიპი გვინდა იყოს ციფრი/სიტყვა.
+print(str("Hello"))
+print(int(6))
+print(float(9.1))
+
+# Explicitly არის როცა პროგრამა თვითონვე ანიჭებს ციფრს/სიტყვას მონაცემთა ტიპს.
+print("Home")
+print(90)
+print(9.99)
