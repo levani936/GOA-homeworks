289221e3ac64c0ed6a1b0de2cb723e488c225c6a
levani936 <le.bedoshvili@yahoo.com>
2025-10-17 00:41:55 -0700

    commit

---
+++ day 15/Classwork/classwork.py
@@ -0,0 +1,13 @@
+# Conditional Statement არის როცა კოდის შინაარსი რაღაც პირობის მიხედვით სრულდება.
+
+# if – თუ
+# elif – სხვა თუ
+# else – ყველა სხვა შემთხვევა
+
+age = 20
+if age > 18:
+    print("You are an adult!")
+elif age == 18:
+    print("You are 18yo!")
+else:
+    print("You are teenager!")
