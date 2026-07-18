c26933f1fe13ecff2b1bd26ea56f14be9e7d381f
levani936 <le.bedoshvili@yahoo.com>
2026-07-18 11:10:19 +0400

    commit

---
+++ day 50/Classwork/classwork.js
@@ -0,0 +1,14 @@
+let name = prompt("Enter your name:")
+console.log(name.startsWith("ლ"));
+console.log(typeof name)
+
+let age = 13
+age = 16
+console.log(age)
+
+
+let age2 = 13;
+let futureAge = age2 + 10;
+console.log(age2);      
+console.log(futureAge);   
+console.log(futureAge % age2);
